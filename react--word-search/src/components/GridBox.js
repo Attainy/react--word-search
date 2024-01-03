34b7e5f547eeaf942d7168a55e2694787599ae3f
Attainy <111291076+Attainy@users.noreply.github.com>
2024-01-03 21:35:16 +0900

    글쓰기 페이지 추가
    글쓰기 버튼 누르면 글쓰기 페이지로 넘어가도록 설정했습니다.

--- react--word-search/src/components/GridBox.js
+++ react--word-search/src/components/GridBox.js
@@ -1,18 +1,14 @@
 import React from 'react'
 
-function GridBox() {
-  const gridCellStyle = {
-    border: "1px solid gray"
-  }
+function GridBox({ givenWords }) {
 
-  const wordarr = ['CHIHUAHUA', 'BULLDOG', 'TERRIER',
-  'COLLIE', 'BOXER', 'HOUND']
+
 
   return (
     <div className='gridBox'>
       {
         [...Array(parseInt(180))].map(() => (
-          <div className='itemBox' style={gridCellStyle}>
+          <div className='itemBox'>
             <div className="item">A</div>
           </div>
         ))
--- react--word-search/src/pages/Game.js
+++ react--word-search/src/pages/Game.js
@@ -2,18 +2,20 @@ import React from 'react';
 import GridBox from '../components/GridBox';
 
 function Game() {
-  const givenWord = ['CHIHUAHUA', 'BULLDOG', 'TERRIER',
-  'COLLIE', 'BOXER', 'HOUND']
+  const givenWords = ['CHIHUAHUA', 'BULLDOG', 'TERRIER',
+  'COLLIE', 'SHEPHERD', 'BOXER', 'HOUND',  'BEAGLE', 
+  'CORGI', 'ROTTWEILER', 'PINSCHER', 'DALMATIAN', 
+  'SETTER', 'MASTIFF', 'DACHSHUND']
   
   return (
     <div className='game'>
       <h2>Subject</h2>
 
       <div className="playbox">
-        <GridBox></GridBox>
+        <GridBox givenWords={givenWords}></GridBox>
         <div className="word-list">
           {
-            givenWord.map((val) => {
+            givenWords.map((val) => {
               return (
                 <div data-name={val}>{val}</div>
               )
